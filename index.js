e6a49512a748c765a44162b0f71deb681cd54273
CharlieRojas <crc_185@yahoo.com>
2022-03-15 23:36:51 -0500

    commit tree 64112c7588449fdea0920f2a83a36fb47c2646a1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+
+// const superbowlWin = record.find(function(record){ 
+// }
+
+function superbowlWin(array1){
+    const resultWin = array1.find(obj => obj.result === 'W');
+    if(resultWin !==undefined){
+        return resultWin.year
+    }else
+    return resultWin
+};
